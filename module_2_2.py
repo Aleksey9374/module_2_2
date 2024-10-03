e26777f19d0285b3c558c157632ddbfed250b459
Aleksey9374 <noyabrskhome75028@gmail.com>
2024-10-03 14:20:46 +0500

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,24 @@
-first = int(input('Введите первое число: '))
-second = int(input('Введите второе число: '))
-third = int(input('Введите третье число: '))
+while True:
+    try:
+        first = int(input('Введите первое число: '))
+        break
+    except ValueError:
+        print('Вы напечатали что-то другое')
+        continue
+while True:
+    try:
+        second = int(input('Введите второе число: '))
+        break
+    except ValueError:
+        print('Вы напечатали что-то другое')
+        continue
+while True:
+    try:
+        third = int(input('Введите третье число: '))
+        break
+    except ValueError:
+        print('Вы напечатали что-то другое')
+        continue
 arr = {first, second, third}
 if len(arr) == 3:
     print(0)
